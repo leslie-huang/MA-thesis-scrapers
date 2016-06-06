f267918febb0084aea8670a66a486f722186e906
Leslie Huang <lesliehuang@gmail.com>
2016-06-05 21:27:24 -0400

    added scraper for 2016 government statements

---
+++ 2016-government-scraper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import csv
+from government_statement_scraping_utilities import get_result_urls, get_links, get_statement
+
+with open("2016statements.csv", "w") as f:
+    writer = csv.writer(f)    
+    writer.writerow(["title", "text", "date"])
+    
+    for result_url in get_result_urls():
+        for link in get_links(result_url):
+            statement = get_statement(link)
+            if statement is not None:
+            
+                writer.writerow([
+                    statement["title"],
+                    statement["text"],
+                    statement["date"]
+                ])
