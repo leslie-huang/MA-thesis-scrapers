9a0b034468386ac1dc01c848120ca65b71ca82f4
Leslie Huang <lesliehuang@gmail.com>
2016-06-06 17:18:31 -0400

    fixed date

--- FARC parser.py
+++ FARC parser.py
@@ -6,6 +6,7 @@ import urllib.request
 
 import json
 import os
+import re
 import string
 import sys
 from os.path import normpath, basename
@@ -19,15 +20,44 @@ print(communique_filenames)
 
 articles_list = []
 
+meses = {
+    "enero": 1,
+    "febrero": 2,
+    "marzo": 3,
+    "abril": 4,
+    "mayo": 5,
+    "junio": 6,
+    "julio": 7,
+    "agosto": 8,
+    "septiembre": 9,
+    "octubre": 10,
+    "noviembre": 11,
+    "diciembre": 12
+}
+
 for filename in communique_filenames:
     webpage = open(os.path.join("communiques", filename)).read()
     soup = BeautifulSoup(webpage, "html.parser")
     
     # find metadata and paragraphs of statement
-    date = soup.find("span", class_="itemDateCreated")
+    dateline = soup.find("span", class_="itemDateCreated")
     title = soup.find("h2", class_="itemTitle")
     author = soup.find("span", class_="itemAuthor")
     text = soup.find("div", class_="itemFullText")
+    
+    # parse word date into numerical date
+    datel = re.search("[0-9]{1,2} [a-z]{1,12} [0-9]{4}", dateline)
+    day = re.search("[0-9]{1,2}", datel)
+    month = re.search("[a-z]{1,12}", datel)
+    for key in meses:
+        if month == key:
+            nummonth = meses[key]
+    year = re.search("[0-9]{4}", datel)
+    
+    def datemaker(year, nummonth, day):
+        return str(year) + "-" + str(nummonth) + "-" + str(day)
+    
+    date = datemaker(year, nummonth, day)
     
     paragraphs = text.find_all("p")
     
